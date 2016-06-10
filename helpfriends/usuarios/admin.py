485abe0247199374ec5ff7802fca4748d7bf1c15
Santos Gallegos <santos_g@outlook.com>
2016-06-10 12:49:15 -0500

    Creadas clases, configiurada db

--- helpfriends/usuarios/admin.py
+++ helpfriends/usuarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Discapacidad
+from .models import PCE
+from .models import TrabajadorPublico
+from .models import Ciudadano
+
+admin.site.register(Discapacidad)
+admin.site.register(PCE)
+admin.site.register(TrabajadorPublico)
+admin.site.register(Ciudadano)
--- helpfriends/usuarios/models.py
+++ helpfriends/usuarios/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Persona(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=10)
+    mail = models.EmailField()
+
+    def __str__(self):
+        return self.id + " " + self.apellido + " " + self.nombre
+
+
+class Voluntario(Persona):
+    pass
+
+
+class Ciudadano(Voluntario):
+    conocimiento_priemerosaux = models.BooleanField(default=False)
+
+
+class TrabajadorPublico(Voluntario):
+    institucion = models.CharField(max_length=250)
+    especialidad = models.CharField(max_length=250)
+
+
+class Discapacidad(models.Model):
+    tipo = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.tipo
+
+
+class PCE(Persona):
+    discapacidad = models.ForeignKey(Discapacidad)
+    porcentaje = models.FloatField()
+
+    def __str__(self):
+        return Persona.__str__(self) + " - " + str(self.discapacidad) + " %" + str(self.porcentaje)
