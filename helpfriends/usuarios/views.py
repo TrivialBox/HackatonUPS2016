79b1c53458df23038c7937d1c7a5295bb45ab4a1
Santos Gallegos <santos_g@outlook.com>
2016-06-10 21:08:36 -0500

    Latitud y longitud

--- helpfriends/usuarios/views.py
+++ helpfriends/usuarios/views.py
@@ -2,19 +2,47 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import PCE
+from .models import Discapacidad
+
 from ipware.ip import get_real_ip
+import geocoder
+import googlemaps
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def accion(request, id, accion):
     # if request.method == 'POST':
-    ip = getip(request)
-    return HttpResponse("<h1>" + str(ip) + "</h1>")
-    # return redirect('/')
+    d = Discapacidad(tipo="Cordura")
+    d.save()
+    # ip = getip(request)
+    # return HttpResponse(get_direccion(ip))
+    return redirect('/')
+
+
+def get_direccion(ip):
+    gmaps = googlemaps.Client(key='AIzaSyBCbMUYaa4h9gNoWAjq-6OKz7i9R3gf8JU')
+    direccion = gmaps.reverse_geocode(getlatlng(ip))
+    return direccion
+
+
+def getlatlng(ip):
+    soup = BeautifulSoup(get_raw_latlng(ip))
+    latitud = soup.find('td', {'id': 'latitud'}).text
+    longitud = soup.find('td', {'id': 'longitud'}).text
+    return latitud, longitud
 
 
 def getip(request):
     ip = get_real_ip(request)
     if ip is not None:
         return ip
-    return 0
+    return '200.110.89.74'
 
+
+def get_raw_latlng(ip):
+    url = 'http://www.cual-es-mi-ip.net/geolocalizar-ip-mapa'
+    data = {'direccion-ip': ip}
+    req = requests.get(url, data=data)
+    return req.text
